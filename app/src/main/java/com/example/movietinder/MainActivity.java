c14195e71802031fa08342c684378923bc9a6492
kwavey <kbirongo@gmail.com>
2020-10-01 13:46:52 +0200

    setting up the project

---
+++ app/src/main/java/com/example/movietinder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.movietinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //hey I feel good
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
