f2797ce9081d5c702999fb1b14d44a68e192036b
kwavey <kbirongo@gmail.com>
2020-10-08 15:12:54 +0200

    Login og authentication

--- app/src/main/java/com/example/movietinder/Login.java
+++ app/src/main/java/com/example/movietinder/Login.java
@@ -1,16 +1,30 @@
 package com.example.movietinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     Button gotomain;
     TextView gotosignup;
+    EditText emailuser, passworduser;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
 
 
 
@@ -19,8 +33,20 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(Login.this, MainActivity.class));
+            finish();
+        }
+
+        //inputter users.
+        emailuser=findViewById(R.id.loginemail);
+        passworduser=findViewById(R.id.loginpassword);
         gotomain=findViewById(R.id.loginbutton);
         gotosignup =findViewById(R.id.textView2);
+        progressBar= findViewById(R.id.progressBar2);
 
 
 
@@ -30,7 +56,48 @@ public class Login extends AppCompatActivity {
         gotomain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(gomain);
+                String email = emailuser.getText().toString();
+                final String password = passworduser.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authentication
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        passworduser.setError("Password too short, enter minimum 6 characters!");
+                                    } else {
+                                        Toast.makeText(Login.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(Login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+
+
+
+
             }
         });
 
@@ -40,6 +107,11 @@ public class Login extends AppCompatActivity {
                 startActivity(gosignup);
             }
         });
+
+
+
+
+
     }
 
 }
